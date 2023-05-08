b46e792b7876e5464c06c99be9924b8e9f1b360b
PetterBostrom <petter.bostrom@gmail.com>
2023-05-08 14:02:24 +0200

    Added models project

---
+++ TimeReportModels/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace TimeReportModels
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public ICollection<TimeReport> TimeReports { get; set; }
+        public ICollection<Project> Projects { get; set; }
+        public ICollection<Project> Projects { get; set; }
+    }
+}
---
+++ TimeReportModels/TimeReport.cs
@@ -0,0 +1,18 @@
+﻿namespace TimeReportModels
+{
+    public class TimeReport
+    {
+        public int TRId { get; set; }
+        public int EmpId { get; set; }
+        public Employee Employee { get; set; }
+        public DateOnly Date { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public TimeSpan WorkHours { get; set; }
+
+        public TimeReport()
+        {
+            WorkHours = End - Start;
+        }
+    }
+}
