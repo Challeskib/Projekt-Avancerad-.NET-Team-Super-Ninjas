85e81dcccba7e66b4b185253cadf9e1e2c5d7a53
PetterBostrom <petter.bostrom@gmail.com>
2023-05-08 16:03:32 +0200

    Fixed DBcontext and seed data

---
+++ Projekt – Avancerad .NET Team Super-Ninjas/Data/DataContext.cs
@@ -0,0 +1,94 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using TimeReportModels;
+
+namespace Projekt___Avancerad_.NET_Team_Super_Ninjas.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<TimeReport> TimeReports { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<EmployeeProject> EmployeeProjects { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee { EmpId = 1, Name = "Kalle Gustavsson", Email = "Kalle@företaget.com" },
+                new Employee { EmpId = 2, Name = "Sara Andersson", Email = "Sara@företaget.com" },
+                new Employee { EmpId = 3, Name = "Johan Svensson", Email = "Johan@företaget.com" },
+                new Employee { EmpId = 4, Name = "Lisa Lindström", Email = "Lisa@företaget.com" },
+                new Employee { EmpId = 5, Name = "Anders Nilsson", Email = "Anders@företaget.com" },
+                new Employee { EmpId = 6, Name = "Emma Bergström", Email = "Emma@företaget.com" },
+                new Employee { EmpId = 7, Name = "Peter Persson", Email = "Peter@företaget.com" },
+                new Employee { EmpId = 8, Name = "Maria Karlsson", Email = "Maria@företaget.com" },
+                new Employee { EmpId = 9, Name = "Erik Johansson", Email = "Erik@företaget.com" },
+                new Employee { EmpId = 10, Name = "Anna Ahlström", Email = "Anna@företaget.com" }
+            );
+
+            modelBuilder.Entity<Project>().HasData(
+                new Project { ProjId = 1, Name = "E-commerce Website", Description = "Develop an online store for a retail company." },
+                new Project { ProjId = 2, Name = "Blog Platform", Description = "Create a web platform for bloggers to publish and share their content." },
+                new Project { ProjId = 3, Name = "Mobile Game App", Description = "Develop a fun and engaging mobile game for iOS and Android platforms." }
+            );
+
+            modelBuilder.Entity<TimeReport>().HasData(
+                new TimeReport(new DateTime(2023, 02, 01, 10, 00, 00), new DateTime(2023, 02, 01, 13, 00, 00)) { TRId = 1, EmployeeId = 1 },
+                new TimeReport(new DateTime(2023, 03, 15, 08, 00, 00), new DateTime(2023, 03, 15, 11, 30, 00)) { TRId = 2, EmployeeId = 2 },
+                new TimeReport(new DateTime(2023, 04, 05, 14, 00, 00), new DateTime(2023, 04, 05, 16, 00, 00)) { TRId = 3, EmployeeId = 3 },
+                new TimeReport(new DateTime(2023, 05, 01, 09, 00, 00), new DateTime(2023, 05, 01, 10, 30, 00)) { TRId = 4, EmployeeId = 4 },
+                new TimeReport(new DateTime(2023, 01, 20, 11, 00, 00), new DateTime(2023, 01, 20, 12, 00, 00)) { TRId = 5, EmployeeId = 5 },
+                new TimeReport(new DateTime(2023, 02, 05, 08, 00, 00), new DateTime(2023, 02, 05, 10, 00, 00)) { TRId = 6, EmployeeId = 6 },
+                new TimeReport(new DateTime(2023, 03, 22, 13, 00, 00), new DateTime(2023, 03, 22, 15, 00, 00)) { TRId = 7, EmployeeId = 7 },
+                new TimeReport(new DateTime(2023, 04, 14, 12, 00, 00), new DateTime(2023, 04, 14, 14, 00, 00)) { TRId = 8, EmployeeId = 8 },
+                new TimeReport(new DateTime(2023, 05, 01, 08, 00, 00), new DateTime(2023, 05, 01, 10, 00, 00)) { TRId = 9, EmployeeId = 9 },
+                new TimeReport(new DateTime(2023, 01, 15, 09, 00, 00), new DateTime(2023, 01, 15, 11, 00, 00)) { TRId = 10, EmployeeId = 10 },
+                new TimeReport(new DateTime(2023, 01, 10, 13, 00, 00), new DateTime(2023, 01, 10, 14, 30, 00)) { TRId = 11, EmployeeId = 1 },
+                new TimeReport(new DateTime(2023, 01, 01, 08, 00, 00), new DateTime(2023, 01, 01, 10, 00, 00)) { TRId = 12, EmployeeId = 1 },
+                new TimeReport(new DateTime(2023, 01, 02, 09, 00, 00), new DateTime(2023, 01, 02, 11, 00, 00)) { TRId = 13, EmployeeId = 2 },
+                new TimeReport(new DateTime(2023, 01, 03, 10, 00, 00), new DateTime(2023, 01, 03, 12, 00, 00)) { TRId = 14, EmployeeId = 3 },
+                new TimeReport(new DateTime(2023, 01, 04, 11, 00, 00), new DateTime(2023, 01, 04, 13, 00, 00)) { TRId = 15, EmployeeId = 4 },
+                new TimeReport(new DateTime(2023, 01, 05, 12, 00, 00), new DateTime(2023, 01, 05, 14, 00, 00)) { TRId = 16, EmployeeId = 5 },
+                new TimeReport(new DateTime(2023, 01, 06, 13, 00, 00), new DateTime(2023, 01, 06, 15, 00, 00)) { TRId = 17, EmployeeId = 6 },
+                new TimeReport(new DateTime(2023, 01, 07, 14, 00, 00), new DateTime(2023, 01, 07, 16, 00, 00)) { TRId = 18, EmployeeId = 7 },
+                new TimeReport(new DateTime(2023, 01, 08, 15, 00, 00), new DateTime(2023, 01, 08, 17, 00, 00)) { TRId = 19, EmployeeId = 8 },
+                new TimeReport(new DateTime(2023, 01, 09, 16, 00, 00), new DateTime(2023, 01, 09, 18, 00, 00)) { TRId = 20, EmployeeId = 9 },
+                new TimeReport(new DateTime(2023, 03, 05, 10, 00, 00), new DateTime(2023, 03, 05, 12, 00, 00)) { TRId = 21, EmployeeId = 1 },
+                new TimeReport(new DateTime(2023, 04, 18, 08, 00, 00), new DateTime(2023, 04, 18, 11, 00, 00)) { TRId = 22, EmployeeId = 2 },
+                new TimeReport(new DateTime(2023, 05, 01, 14, 00, 00), new DateTime(2023, 05, 01, 16, 00, 00)) { TRId = 23, EmployeeId = 3 },
+                new TimeReport(new DateTime(2023, 01, 25, 12, 00, 00), new DateTime(2023, 01, 25, 14, 00, 00)) { TRId = 24, EmployeeId = 4 },
+                new TimeReport(new DateTime(2023, 02, 10, 09, 00, 00), new DateTime(2023, 02, 10, 11, 00, 00)) { TRId = 25, EmployeeId = 5 },
+                new TimeReport(new DateTime(2023, 03, 25, 08, 00, 00), new DateTime(2023, 03, 25, 11, 00, 00)) { TRId = 26, EmployeeId = 6 },
+                new TimeReport(new DateTime(2023, 04, 10, 14, 00, 00), new DateTime(2023, 04, 10, 15, 30, 00)) { TRId = 27, EmployeeId = 7 },
+                new TimeReport(new DateTime(2023, 05, 01, 08, 00, 00), new DateTime(2023, 05, 01, 09, 30, 00)) { TRId = 28, EmployeeId = 8 },
+                new TimeReport(new DateTime(2023, 01, 20, 13, 00, 00), new DateTime(2023, 01, 20, 14, 00, 00)) { TRId = 29, EmployeeId = 9 },
+                new TimeReport(new DateTime(2023, 02, 05, 10, 00, 00), new DateTime(2023, 02, 05, 12, 00, 00)) { TRId = 30, EmployeeId = 10 });
+
+            modelBuilder.Entity<EmployeeProject>().HasKey(e => new {e.ProjId, e.EmpId});
+            modelBuilder.Entity<EmployeeProject>().HasData(
+                new {ProjId = 1, EmpId = 1 },
+                new {ProjId = 1, EmpId = 2 },
+                new {ProjId = 1, EmpId = 3 },
+                new {ProjId = 1, EmpId = 4 },
+                new {ProjId = 2, EmpId = 5 },
+                new {ProjId = 2, EmpId = 6 },
+                new {ProjId = 2, EmpId = 7 },
+                new {ProjId = 2, EmpId = 8 },
+                new {ProjId = 3, EmpId = 9 },
+                new { ProjId = 3, EmpId = 10 },
+                new { ProjId = 3, EmpId = 4 },
+                new { ProjId = 3, EmpId = 5 },
+                new { ProjId = 2, EmpId = 2 },
+                new { ProjId = 2, EmpId = 3 },
+                new { ProjId = 2, EmpId = 10 }
+                );
+
+        }
+    }
+}
---
+++ Projekt – Avancerad .NET Team Super-Ninjas/Migrations/20230508140130_init create.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Projekt___Avancerad_.NET_Team_Super_Ninjas.Migrations
+{
+    /// <inheritdoc />
+    public partial class initcreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmployeeProjects",
+                columns: table => new
+                {
+                    EmpId = table.Column<int>(type: "int", nullable: false),
+                    ProjId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeProjects", x => new { x.ProjId, x.EmpId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmpId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmpId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    ProjId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.ProjId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TimeReports",
+                columns: table => new
+                {
+                    TRId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeId = table.Column<int>(type: "int", nullable: false),
+                    Start = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    End = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    WorkHours = table.Column<TimeSpan>(type: "time", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimeReports", x => x.TRId);
+                    table.ForeignKey(
+                        name: "FK_TimeReports_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "EmpId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeProject",
+                columns: table => new
+                {
+                    EmployeesEmpId = table.Column<int>(type: "int", nullable: false),
+                    ProjectsProjId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeProject", x => new { x.EmployeesEmpId, x.ProjectsProjId });
+                    table.ForeignKey(
+                        name: "FK_EmployeeProject_Employees_EmployeesEmpId",
+                        column: x => x.EmployeesEmpId,
+                        principalTable: "Employees",
+                        principalColumn: "EmpId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EmployeeProject_Projects_ProjectsProjId",
+                        column: x => x.ProjectsProjId,
+                        principalTable: "Projects",
+                        principalColumn: "ProjId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "EmployeeProjects",
+                columns: new[] { "EmpId", "ProjId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 2, 1 },
+                    { 3, 1 },
+                    { 4, 1 },
+                    { 2, 2 },
+                    { 3, 2 },
+                    { 5, 2 },
+                    { 6, 2 },
+                    { 7, 2 },
+                    { 8, 2 },
+                    { 10, 2 },
+                    { 4, 3 },
+                    { 5, 3 },
+                    { 9, 3 },
+                    { 10, 3 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "EmpId", "Email", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Kalle@företaget.com", "Kalle Gustavsson" },
+                    { 2, "Sara@företaget.com", "Sara Andersson" },
+                    { 3, "Johan@företaget.com", "Johan Svensson" },
+                    { 4, "Lisa@företaget.com", "Lisa Lindström" },
+                    { 5, "Anders@företaget.com", "Anders Nilsson" },
+                    { 6, "Emma@företaget.com", "Emma Bergström" },
+                    { 7, "Peter@företaget.com", "Peter Persson" },
+                    { 8, "Maria@företaget.com", "Maria Karlsson" },
+                    { 9, "Erik@företaget.com", "Erik Johansson" },
+                    { 10, "Anna@företaget.com", "Anna Ahlström" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Projects",
+                columns: new[] { "ProjId", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Develop an online store for a retail company.", "E-commerce Website" },
+                    { 2, "Create a web platform for bloggers to publish and share their content.", "Blog Platform" },
+                    { 3, "Develop a fun and engaging mobile game for iOS and Android platforms.", "Mobile Game App" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "TimeReports",
+                columns: new[] { "TRId", "EmployeeId", "End", "Start", "WorkHours" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2023, 2, 1, 13, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 2, 1, 10, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 3, 0, 0, 0) },
+                    { 2, 2, new DateTime(2023, 3, 15, 11, 30, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 3, 15, 8, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 3, 30, 0, 0) },
+                    { 3, 3, new DateTime(2023, 4, 5, 16, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 4, 5, 14, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0) },
+                    { 4, 4, new DateTime(2023, 5, 1, 10, 30, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 5, 1, 9, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 1, 30, 0, 0) },
+                    { 5, 5, new DateTime(2023, 1, 20, 12, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 1, 20, 11, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 1, 0, 0, 0) },
+                    { 6, 6, new DateTime(2023, 2, 5, 10, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 2, 5, 8, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0) },
+                    { 7, 7, new DateTime(2023, 3, 22, 15, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 3, 22, 13, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0) },
+                    { 8, 8, new DateTime(2023, 4, 14, 14, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 4, 14, 12, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0) },
+                    { 9, 9, new DateTime(2023, 5, 1, 10, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 5, 1, 8, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0) },
+                    { 10, 10, new DateTime(2023, 1, 15, 11, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 1, 15, 9, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0) },
+                    { 11, 1, new DateTime(2023, 1, 10, 14, 30, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 1, 10, 13, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 1, 30, 0, 0) },
+                    { 12, 1, new DateTime(2023, 1, 1, 10, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 1, 1, 8, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0) },
+                    { 13, 2, new DateTime(2023, 1, 2, 11, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 1, 2, 9, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0) },
+                    { 14, 3, new DateTime(2023, 1, 3, 12, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 1, 3, 10, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0) },
+                    { 15, 4, new DateTime(2023, 1, 4, 13, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 1, 4, 11, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0) },
+                    { 16, 5, new DateTime(2023, 1, 5, 14, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 1, 5, 12, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0) },
+                    { 17, 6, new DateTime(2023, 1, 6, 15, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 1, 6, 13, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0) },
+                    { 18, 7, new DateTime(2023, 1, 7, 16, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 1, 7, 14, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0) },
+                    { 19, 8, new DateTime(2023, 1, 8, 17, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 1, 8, 15, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0) },
+                    { 20, 9, new DateTime(2023, 1, 9, 18, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 1, 9, 16, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0) },
+                    { 21, 1, new DateTime(2023, 3, 5, 12, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 3, 5, 10, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0) },
+                    { 22, 2, new DateTime(2023, 4, 18, 11, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 4, 18, 8, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 3, 0, 0, 0) },
+                    { 23, 3, new DateTime(2023, 5, 1, 16, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 5, 1, 14, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0) },
+                    { 24, 4, new DateTime(2023, 1, 25, 14, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 1, 25, 12, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0) },
+                    { 25, 5, new DateTime(2023, 2, 10, 11, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 2, 10, 9, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0) },
+                    { 26, 6, new DateTime(2023, 3, 25, 11, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 3, 25, 8, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 3, 0, 0, 0) },
+                    { 27, 7, new DateTime(2023, 4, 10, 15, 30, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 4, 10, 14, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 1, 30, 0, 0) },
+                    { 28, 8, new DateTime(2023, 5, 1, 9, 30, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 5, 1, 8, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 1, 30, 0, 0) },
+                    { 29, 9, new DateTime(2023, 1, 20, 14, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 1, 20, 13, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 1, 0, 0, 0) },
+                    { 30, 10, new DateTime(2023, 2, 5, 12, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2023, 2, 5, 10, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 2, 0, 0, 0) }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeProject_ProjectsProjId",
+                table: "EmployeeProject",
+                column: "ProjectsProjId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimeReports_EmployeeId",
+                table: "TimeReports",
+                column: "EmployeeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeProject");
+
+            migrationBuilder.DropTable(
+                name: "EmployeeProjects");
+
+            migrationBuilder.DropTable(
+                name: "TimeReports");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
