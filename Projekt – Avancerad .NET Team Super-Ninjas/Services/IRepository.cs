49e43932aeadf05816ec7f5d77e452c7a61e9f78
Charlie Skibsted <charlieskibsted@gmail.com>
2023-05-09 09:00:52 +0200

    Added IRepository Generic for the repository pattern

---
+++ Projekt – Avancerad .NET Team Super-Ninjas/Services/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Projekt___Avancerad_.NET_Team_Super_Ninjas.Services
+{
+    public interface IRepository<T>
+    {
+
+        public Task<T> GetAll();
+        public Task<T> GetById(int id);
+        public Task<T> Create(T obj);
+        public Task<T> Update(T obj);  
+        public Task<T> Delete(int id);
+
+    }
+}
