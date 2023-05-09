3bd8d54be5f6dabbeea331a6cb1b563541e6ee86
Charlie Skibsted <charlieskibsted@gmail.com>
2023-05-09 09:07:03 +0200

    Added EmployeeRepo EmployeeController

---
+++ Projekt – Avancerad .NET Team Super-Ninjas/Services/EmployeeRepository.cs
@@ -0,0 +1,30 @@
+﻿namespace Projekt___Avancerad_.NET_Team_Super_Ninjas.Services
+{
+    public class EmployeeRepository : IRepository<Employee>
+    {
+        public Task<Employee> Create(Employee obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> Update(Employee obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
